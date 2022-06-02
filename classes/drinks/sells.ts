5b18b3f6f7c38d01d1c1ebec2afa1359e38d2e32
bryanPerez <bam.perez@yavirac.edu.ec>
2022-06-02 15:52:38 -0500

    Doing mixed drinks

--- classes/drinks/sells.ts
+++ classes/drinks/sells.ts
@@ -9,16 +9,30 @@ export class Sells {
 
     sellsList: Sell[] = [];
 
-    sellDrink(flavor: string, size: string) {
-        const AreIngredientsAvailable = this.inventory.checkStock(flavor,size);
-        if (AreIngredientsAvailable) {
-            const drink = new Drink(1, flavor, size);
-            const sell = new Sell(drink, 1);
-            this.sellsList.push(sell)
-            this.inventory.updateStock(drink);
-            return 'Drink Selled'
-        }
+    sellDrink(flavor: string[], size: string): string {
+        if ( flavor.length === 1) {
+            const flavorString = flavor[0]; 
+            
+            const AreIngredientsAvailable = this.inventory.checkStock(flavorString, size);
+
+            if (AreIngredientsAvailable) {
+                const drink = new Drink(1, flavorString, size);
+                const sell = new Sell(drink, 1);
+                this.sellsList.push(sell)
+                this.inventory.updateStock(drink);
+                return 'Drink Selled'
+            }
+            return 'There is no ingredients enough'
+        } 
+
+        
+
+
         return 'There is no ingredients enough'
- 
+        
+
     }
+
+
+
 }
